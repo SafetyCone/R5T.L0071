61f6ff38aee8884725e3041e82b2b4a6bf387057
davidcoats <david@getsafetycone.com>
2024-05-30 22:24:19 -0700

    Interim changes.

--- source/R5T.L0071/Code/Functionality/IConstructionOperator.cs
+++ source/R5T.L0071/Code/Functionality/IConstructionOperator.cs
@@ -1,7 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 using R5T.T0132;
 
@@ -9,54 +6,9 @@ using R5T.T0132;
 namespace R5T.L0071
 {
     [FunctionalityMarker]
-    public partial interface IConstructionOperator : IFunctionalityMarker
+    public partial interface IConstructionOperator : IFunctionalityMarker,
+        L0066.IConstructionOperator
     {
-        public TValue Create<TValue>(
-           Func<TValue> constructor,
-           params Action<TValue>[] modifiers)
-        {
-            var value = this.Create(
-                constructor,
-                modifiers.AsEnumerable());
-
-            return value;
-        }
-
-        public TValue Create<TValue>(
-            Func<TValue> constructor,
-            IEnumerable<Action<TValue>> modifiers)
-        {
-            var value = constructor();
-
-            Instances.ActionOperator.Run_Actions(
-                value,
-                modifiers);
-
-            return value;
-        }
-
-        public async Task<TValue> Create<TValue>(
-            Func<Task<TValue>> constructor,
-            params Func<TValue, Task>[] modifiers)
-        {
-            var value = await constructor();
-
-            await Instances.ActionOperator.Run(
-                value,
-                modifiers);
-
-            return value;
-        }
-
-        public async Task<TValue> New<TValue>(
-            Func<Task<TValue>> constructor,
-            params Func<TValue, Task>[] modifiers)
-        {
-            var value = await this.Create(
-                constructor,
-                modifiers);
-
-            return value;
-        }
+        
     }
 }
